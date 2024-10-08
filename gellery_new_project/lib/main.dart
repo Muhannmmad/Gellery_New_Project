2a66018bb54f7d6a555b1c9c87f04dbb8fd0d309
Muhammad <muhannmmad@gmail.com>
2024-10-08 13:31:30 +0200

    appbar and bottom navigation bar made

--- gellery_new_project/lib/main.dart
+++ gellery_new_project/lib/main.dart
@@ -12,3 +12,34 @@ class MyGalleryApp extends StatelessWidget {
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 98, 0, 57),
+        title: Text('MyGallery'),
+        foregroundColor: Colors.white,
+        titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+      body: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image),
+            label: 'Bilder',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Über mich',
+          ),
+        ],
+      ),
+    );
+  }
+}
