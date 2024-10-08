5f7e14f6d9928e91989c0a3f666ed8fe6c81f0de
Muhammad <muhannmmad@gmail.com>
2024-10-08 13:20:37 +0200

    my gallery app

--- gellery_new_project/lib/main.dart
+++ gellery_new_project/lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyGalleryApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyGalleryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: HomePage(),
     );
   }
 }
