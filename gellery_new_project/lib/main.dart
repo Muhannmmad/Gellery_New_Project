1a9a884e1791c82fdff36125280fbdcdaacea4b5
Muhammad <muhannmmad@gmail.com>
2024-10-09 08:53:28 +0200

    all routes for all pages saved in materialapp

--- gellery_new_project/lib/main.dart
+++ gellery_new_project/lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:gellery_new_project/screens/profile.dart';
+import 'package:gellery_new_project/screens/screen1.dart';
+import 'package:gellery_new_project/screens/screen2.dart';
+import 'package:gellery_new_project/screens/screen3.dart';
+import 'package:gellery_new_project/screens/screen4.dart';
+import 'package:gellery_new_project/screens/screen5.dart';
+import 'package:gellery_new_project/screens/screen6.dart';
+import 'package:gellery_new_project/screens/screen7.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +16,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MyGalleryPage(),
+        '/profile': (context) => ProfilePage(),
+        '/1': (context) => Bunter(),
+        '/2': (context) => Adrenalin(),
+        '/3': (context) => Achtung(),
+        '/4': (context) => Runners(),
+        '/5': (context) => Swish(),
+        '/6': (context) => Ball(),
+        '/7': (context) => Sieg(),
+      },
       title: 'MyGallery',
       home: MyGalleryPage(),
     );
